0554e5690495e10363fe5f38590ad8a491d65f26
Aleksandr Kurdiukov <ak.moabit@gmail.com>
2022-01-12 11:31:20 +0100

    Initial Commit

---
+++ GH-Repos/ReposViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ReposViewController.swift
+//  GH-Repos
+//
+//  Created by Aleksandr Kurdiukov on 12.01.22.
+//
+
+import UIKit
+
+class ReposViewController: UIViewController {
+
+    //MARK: - Interface
+    
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - LifeCycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+    
+    
+    //MARK: - Methods
+    
+    
+    //MARK: - Extensions
+
+
+
+}
+
---
+++ GH-Repos/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  GH-Repos
+//
+//  Created by Aleksandr Kurdiukov on 12.01.22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        
+        window?.rootViewController = ReposViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+
+}
+
