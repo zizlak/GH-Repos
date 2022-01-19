471c01b055a9736363e319dc3ea33ceda0246af8
Aleksandr Kurdiukov <ak.moabit@gmail.com>
2022-01-19 10:00:19 +0100

    pods

---
+++ Pods/Target Support Files/Pods-GH-Repos-GH-ReposUITests/Pods-GH-Repos-GH-ReposUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GH_Repos_GH_ReposUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GH_Repos_GH_ReposUITests
+@end
---
+++ Pods/Target Support Files/Pods-GH-ReposTests/Pods-GH-ReposTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GH_ReposTests : NSObject
+@end
+@implementation PodsDummy_Pods_GH_ReposTests
+@end
